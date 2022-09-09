0beab10de5a916b480e8ba3c4a7a07c911134d5f
anhoa-vu <vah.2706@gmail.com>
2022-09-09 20:46:37 +0930

    add connect to wallet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-
+import detectEthereumProvider from '@metamask/detect-provider';
+import { useState } from 'react';
+  
 function App() {
 
-  const connect = () => {
-    alert('connect');
+  const [account, setAccount] = useState(false);
+
+  const connect =  async() => {
+    try {
+      const provider = await detectEthereumProvider();
+      //return array of accounts
+      const accounts = await provider.request({method: "eth_requestAccounts"});
+
+      //check if array at least one element
+      if (accounts.length > 0){
+        console.log('account found', accounts);
+        setAccount(account[0]);
+        console(account);
+      } else {
+        console.log('No account found');
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  
+
   return (
     <div className="App">
       <header className="App-header">
